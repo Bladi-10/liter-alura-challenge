b96e0c618d389849d01ba265f7d663967f34370e
bladi-10 <lopezhumbertbladimir425@gmail.com>
2025-07-12 22:09:41 -0600

    Inicio del proyecto

---
+++ src/main/java/com/bladi/_k/liter_alura_challenge/ChallengeLiterAluraBladiApplication.java
@@ -0,0 +1,13 @@
+package com.bladi._k.liter_alura_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeLiterAluraBladiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeLiterAluraBladiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bladi/_k/liter_alura_challenge/ChallengeLiterAluraBladiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bladi._k.liter_alura_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeLiterAluraBladiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
